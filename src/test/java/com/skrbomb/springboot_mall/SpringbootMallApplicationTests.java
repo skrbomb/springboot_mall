b69f3d98414a8c9ce88cd923e65d716a88a7bb5b
skrbomb <clcc620@gmail.com>
2024-07-08 20:18:22 +0800

    初始化 SpringBoot project

---
+++ src/test/java/com/skrbomb/springboot_mall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrbomb.springboot_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
